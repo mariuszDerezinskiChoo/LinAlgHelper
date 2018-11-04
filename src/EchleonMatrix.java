d95a6ed27e30895f2f8709e80767bbc8a876eccb
Daniel Hwang <dhwang314@gmail.com>
2018-11-03 23:06:18 -0400

    Updated echleonmatrix and matrix
    Added multiply and addition functions, cleaned up a bunch of code

--- src/EchleonMatrix.java
+++ src/EchleonMatrix.java
@@ -1,6 +1,9 @@
+/**
+ * This converts a matrix into a valid echleon form
+ * Which you can then use for your own purposes.
+ * @author inSaneSyntax
+ */
 public class EchleonMatrix extends Matrix {
-    double[][] echelonMatrixVar;
-    int rows, columns;
     EchleonMatrix(){
         super();
     }
@@ -21,4 +24,99 @@ public class EchleonMatrix extends Matrix {
             currentRow++;
         }
     }
+    /**
+     * returns the values in a given row in the form of a double array
+     * @param row - # of row to be returned
+     * @return returns a copy of the row
+     */
+    public double[] accessRow(int row){
+        double[] doubles = Arrays.copyOf(matrixVar[row], matrixVar[row].length);
+        return doubles;
+    }
+    /**
+     * do i have to explain?
+     * @param columnNumber # of column to search
+     * @return the first entry in the given column, -1 if not found
+     */
+    public int returnFirstEntryInColumn(int columnNumber){
+        for (int i = 0; i < rows; i++)
+        {
+            if (matrixVar[i][columnNumber] != 0)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * uhhh i'm too lazy to explain this one
+     * @param rowNumber - # of row to search
+     * @return the first entry in the given row, -1 if not found
+     */
+    public int returnFirstEntryInRow(int rowNumber){
+        for (int i = 0; i < columns, i++)
+        {
+            if (matrixVar[rowNumber][i] != 0)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Repeatedly performs ERO #3 to ensure the entries below a pivot are 0.
+     * @param row
+     * @param pivot
+     */
+    public void clearOutColumnUsingPivot(int row, int pivot)
+    {
+        for (int i = pivot+1; i < rows; i++)
+        {
+            double scalar = -matrixVar[row][i]/matrixVar[row][pivot];
+            addMultipleOfRowToAnother(scalar, i, pivot);
+        }
+    }
+    /**
+     * ERO #1. Swaps two rows
+     * @param rowOne # of row to be swapped
+     * @param rowTwo # of row to be swapped
+     */
+    public void swapRows(int rowOne, int rowTwo){
+        double[] temp = accessRow(rowOne);
+        for (int i = 0; i < columns; i++) {
+            matrixVar[rowOne][i] = matrixVar[rowTwo][i];
+            matrixVar[rowTwo][i] = temp[i];
+        }
+    }
+
+    /**
+     * ERO #2. Multiplies a row by a scalar multiple.
+     * @param scalarMultiple - scalar multiple
+     * @param rowNumber - # of row to be modified
+     */
+    public void scalarMultipleOfRow (double scalarMultiple, int rowNumber)
+    {
+        if (scalarMultiple != 0)
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                matrixVar[rowNumber][i] *= scalarMultiple;
+            }
+        }
+    }
+
+    /**
+     * ERO #3. Adds a scalar multiple of a row to another.
+     * @param scalarMultiple - scalar multiple
+     * @param rowToBeAdded - # of row to be added upon to another
+     * @param rowToBeModified - # of row to be modified
+     */
+    public void addMultipleOfRowToAnother(double scalarMultiple, int rowToBeAdded, int rowToBeModified){
+        for (int i = 0; i< columns; i++){
+            matrixVar[rowToBeModified][i] += scalarMultiple * matrixVar[rowToBeAdded][i];
+        }
+    }
+
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,89 +1,81 @@
 import java.util.Arrays;
 
+/**
+ * A mutable matrix.
+ * values stored in matrixVar.
+ * add and multiply functions do not create a new Matrix object.
+ * inverse function creates a new Matrix object.
+ *
+ * something created for fun, instead of studying for linear algebra.
+ * @author inSaneSyntax
+ */
+
 public class Matrix {
     double[][] matrixVar;
     int rows, columns;
+    /**
+     * creates a new 1x1 matrix with an empty value
+     */
     Matrix(){
-        matrixVar = new double[][];
-        rows = 0;
-        columns = 0;
+        matrixVar = new double[1][1];
+        rows = 1;
+        columns = 1;
     }
-    Matrix(double[][] input){
+    /**
+     * creates a new Matrix class from a set of doubles (in matrix form).
+     * @param input
+     */
+    Matrix(double[][] input) {
         matrixVar = input;
         columns = matrixVar[0].length;
         rows = matrixVar.length;
     }
-    Matrix(double[][] one, double[][] two)
-    {
-        rows = one.length;
-        columns = two[0].length;
-        int middleDimension = one[0].length;
+    /**
+     * adds other Matrix of the same dimensions to our current matrix
+     * @param other - a Matrix of the same class
+     */
+    public void addOtherMatrix(Matrix other) {
+        if (other.rows != rows || other.columns != columns) {
+            return;
+        }
+        matrixVar = matrixVar + other.matrixVar;
+    }
+    /**
+     * returns a new Matrix that is the result of multiplying another Matrix with appropriate dimensions with our current Matrix
+     * say that other = B, and our current matrix is A. The result is the matrix BA.
+     * @param other - an other Matrix to multiply at the front of our current matrix, with appropriate dimensions
+     */
+    public Matrix multiplyOtherMatrixToFront(Matrix other) {
+        Matrix output = Matrix(rows, other.columns);
         for (int a = 0; a < rows; a++)
         {
-            for (int b = 0; b < columns; b++)
+            for (int b = 0; b < other.columns; b++)
             {
-                for (int k = 0; k < middleDimension; k++)
+                for (int k = 0; k < columns; k++)
                 {
-                    matrixVar[a][b] += one[a][k] * two[k][b];
+                    output[a][b] = output[a][b] + output[a][k] * other.output[k][b];
                 }
             }
         }
+        return output;
     }
-    public double[] accessRow(int row){
-        double[] doubles = Arrays.copyOf(matrixVar[row], matrixVar[row].length);
-        return doubles;
-    }
-    public void swapRows(int rowOne, int rowTwo){
-        double[] temp = accessRow(rowOne);
-        for (int i = 0; i < columns; i++) {
-            matrixVar[rowOne][i] = matrixVar[rowTwo][i];
-            matrixVar[rowTwo][i] = temp[i];
-        }
-    }
-    public void scalarMultipleOfRow (double scalarMultiple, int rowNumber)
-    {
-        if (scalarMultiple != 0)
+    /**
+     * returns a new Matrix that is the result of multiplying our current Matrix with another Matrix with appropriate dimensions
+     * say that other = B, and our current matrix is A. The result is the matrix AB.
+     * @param other - an other Matrix to multiply at the end of our current matrix, with appropriate dimensions
+     */
+    public void multiplyOtherMatrixToEnd(Matrix other){
+        Matrix output = Matrix(other.rows, columns);
+        for (int a = 0; a < other.rows; a++)
         {
-            for (int i = 0; i < columns; i++)
+            for (int b = 0; b < columns; b++)
             {
-                matrixVar[rowNumber][i] *= scalarMultiple;
+                for (int k = 0; k < other.columns; k++)
+                {
+                    output[a][b] = output[a][b] + output[a][k] * other.output[k][b];
+                }
             }
         }
+        return output;
     }
-    public void addMultipleOfRowToAnother(double scalarMultiple, int rowToBeAdded, int rowToBeModified){
-        for (int i = 0; i< columns; i++){
-            matrixVar[rowToBeModified][i] += scalarMultiple * matrixVar[rowToBeAdded][i];
-        }
-    }
-    public int returnFirstEntryInColumn(int columnNumber){
-        for (int i = 0; i < rows; i++)
-        {
-            if (matrixVar[i][columnNumber] != 0)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public int returnFirstEntryInRow(int rowNumber){
-        for (int i = 0; i < columns, i++)
-        {
-            if (matrixVar[rowNumber][i] != 0)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public void clearOutColumnUsingPivot(int row, int pivot)
-    {
-        for (int i = pivot+1; i < rows; i++)
-        {
-            double scalar = -matrixVar[row][i]/matrixVar[row][pivot];
-            addMultipleOfRowToAnother(scalar, i, pivot);
-        }
-    }
-
-
-
 }
